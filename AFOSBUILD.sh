10e6cde07b3842cedf2b3427d36f8af9f7d71dd5
TCT Source Code TEAM <source@thecrackertechnology.com>
2021-03-18 16:27:35 -0300

    Fix AFOSBUILD.sh

--- AFOSBUILD.sh
+++ AFOSBUILD.sh
@@ -4,9 +4,8 @@ mkdir /opt/ANDRAX/redsocks2
 cp -Rf andraxbin/* /opt/ANDRAX/bin/
 chmod -R 755 /opt/ANDRAX/bin/*
 
-git apply patches/disable-ss.patch
+make DISABLE_SHADOWSOCKS=true
 
-make
 strip redsocks2
 
 cp -Rf redsocks2  redsocks2.service  redsocks.conf.example /opt/ANDRAX/redsocks2
